60d3b156306596c80082c74dddd5d751db92208e
Christian Rocha <schettinechristian@gmail.com>
2025-05-16 00:16:32 -0300

    Revert "250 funcionando"
    This reverts commit 1f441687dfd2596011e7bbda4ab48621ed5ee379.

--- src/components/MovieList/index.tsx
+++ src/components/MovieList/index.tsx
@@ -1,7 +1,7 @@
-'use client';
+'use client'
 
 import { useEffect, useState } from 'react';
-import './index.scss';
+import './index.scss'
 import axios from 'axios';
 import MovieCard from '../MovieCard';
 import { Movie } from '@/types/movie';
@@ -10,53 +10,33 @@ export default function MovieList() {
     const [movies, setMovies] = useState<Movie[]>([]);
 
     useEffect(() => {
-        getMoviesUpTo250();
+        getMovies();
     }, []);
 
-    const getMoviesUpTo250 = async () => {
-        const API_KEY = '0ed637ff0d43f711ced0dfaac63b50ec';
-        const BASE_URL = 'https://api.themoviedb.org/3/movie/top_rated';
-
-        let allMovies: Movie[] = [];
-        let page = 1;
-        let totalPages = 1;
-
-        while (allMovies.length < 250 && page <= totalPages) {
-            try {
-                const response = await axios.get(BASE_URL, {
-                    params: {
-                        api_key: API_KEY,
-                        language: 'pt-BR',
-                        page: page,
-                    }
-                });
-
-                const data = response.data;
-                totalPages = data.total_pages;
-
-                if (data.results) {
-                    allMovies = allMovies.concat(data.results);
-                }
-
-                page++;
-            } catch (error) {
-                console.error(`Erro ao buscar página ${page}:`, error);
-                break;
+    const getMovies = () => {
+        axios({
+            method: 'get',
+            url: 'https://api.themoviedb.org/3/movie/top_rated',
+            params: {
+                api_key: '0ed637ff0d43f711ced0dfaac63b50ec',
+                language: 'pt-BR',
+                page: '1',
             }
-        }
+        }).then(response => {
+            setMovies(response.data.results);
+        })
+    }
 
-        // Garante que terá no máximo 250 filmes
-        setMovies(allMovies.slice(0, 250));
-    };
+    getMovies();
 
-    return (
+    return(
         <ul className="movie-list">
-            {movies.map((movie) => (
+            {movies.map((movie) =>
                 <MovieCard
                     key={movie.id}
                     movie={movie}
                 />
-            ))}
+            )}
         </ul>
     );
 }
